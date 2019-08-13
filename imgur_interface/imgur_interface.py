a582df5569b2efbb8f0f1b00d61ad7f28645495a
Riley Weber <rileyw13@protonmail.com>
2019-08-13 01:26:50 -0400

    refresh_access_token now works

---
+++ imgur_interface/imgur_interface.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import json
+import os
+import requests
+import sys
+
+class ImgurInterface:
+    def __init__(self):
+        self.auth_filename = os.path.abspath(os.path.join(os.path.dirname(__file__), 'auth.json'))
+        self.keys = {}
+        self.get_all_keys_from_json()
+
+    def get_all_keys_from_json(self):
+        with open(self.auth_filename, 'r') as f:
+            keys = json.load(f)
+        self.keys = keys
+
+    def set_all_key_in_json(self):
+        with open(self.auth_filename, 'w') as f:
+            json.dump(self.keys, f)
+
+    def refresh_access_token(self):
+        access_token_url = 'https://api.imgur.com/oauth2/token'
+        request_body = {
+            'refresh_token': self.keys['refreshToken'],
+            'client_id': self.keys['clientId'],
+            'client_secret': self.keys['clientSecret'],
+            'grant_type': 'refresh_token'
+        }
+        r = requests.post(access_token_url, json=request_body)
+        response_json = json.loads(r.text)
+        self.keys['access_token'] = response_json['access_token']
+        # pretty sure we don't have to update this..... but leaving a comment
+        # in for now in case I'm wrong
+        # self.keys['refresh_token'] = response_json['refresh_token']
+        self.set_all_key_in_json()
+        
+    def get_rising_gifs(self):
+        rising_gallery_url = 'https://api.imgur.com/3/gallery/user/rising/day/1?album_previews=true'
+        headers = {
+            'accessToken': ''
+        }
+        # r = requests.get(rising_gallery_url, 
+
--- tests/context.py
+++ tests/context.py
@@ -4,7 +4,6 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-# import imgur_interface
 import imgur_interface.imgur_interface as ii
 import reverse_gifs
 import imgur_gif_reversal_bot
