d84b453222d8de6f605f3db479d8f64b87bdff07
Riley Weber <rileyw13@protonmail.com>
2019-08-15 13:41:19 -0400

    setting up automated tests with sample data

--- tests/test_imgur_interface.py
+++ tests/test_imgur_interface.py
@@ -3,13 +3,26 @@
 
 from .context import reverse_gifs
 
+import os
+import json
 import unittest
 
-
 class TestImgurInterface(unittest.TestCase):
-    """Basic test cases."""
+    @classmethod
+    def setUpClass(cls):
+        cls.SAMPLE_RISING_GALLERY_RESPONSE_FILENAME = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), 
+                         'sample-data/rising-gallery.json')
+            )
+
+    def setUp(self):
+        filename = TestImgurInterface.SAMPLE_RISING_GALLERY_RESPONSE_FILENAME
+        with open(filename, 'r') as f:
+            self.sample_rising_gallery_response = json.load(f)
+
 
     def test_image_is_gif(self):
+        print(self.sample_rising_gallery_response)
         assert True
 
     def test_filter_gifs(self):
