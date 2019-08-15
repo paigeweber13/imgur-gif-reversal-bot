8c830a580d0aaf2f03055563d73c45c88842eb28
Riley Weber <rileyw13@protonmail.com>
2019-08-15 13:55:15 -0400

    wrote two tests for function used to filter

--- tests/test_imgur_interface.py
+++ tests/test_imgur_interface.py
@@ -21,8 +21,18 @@ class TestImgurInterface(unittest.TestCase):
             self.sample_rising_gallery_response = json.load(f)
         self.interface = ii.ImgurInterface()
 
-
     def test_image_is_gif(self):
+        should_be_gif = self.sample_rising_gallery_response['data'][13]
+        # line 1368 in rising-gallery.json
+        self.assertEqual(should_be_gif['title'], 'Hungry Squirrel')
+        self.assertEqual(should_be_gif['images'][0]['type'], 'image/gif')
+        assert True
+
+    def test_image_is_video(self):
+        should_be_video = self.sample_rising_gallery_response['data'][22]
+        # line 2373 in rising-gallery.json
+        self.assertEqual(should_be_video['title'], 'Holy Shit')
+        self.assertEqual(should_be_video['images'][0]['type'], 'video/mp4')
         assert True
 
     def test_filter_gifs(self):
