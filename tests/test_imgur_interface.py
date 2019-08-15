dbddcbbac88bb60d29338ab1c0af22b7edf08f77
Riley Weber <rileyw13@protonmail.com>
2019-08-14 21:40:26 -0400

    created test stubs

---
+++ tests/test_imgur_interface.py
@@ -0,0 +1,20 @@
+
+# -*- coding: utf-8 -*-
+
+from .context import reverse_gifs
+
+import unittest
+
+
+class TestImgurInterface(unittest.TestCase):
+    """Basic test cases."""
+
+    def test_image_is_gif(self):
+        assert True
+
+    def test_filter_gifs(self):
+        assert True
+
+
+if __name__ == '__main__':
+    unittest.main()
