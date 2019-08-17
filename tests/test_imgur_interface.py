7a2078efb98b1cdbe538a7d8accd00ba4ae9642f
Riley Weber <rileyw13@protonmail.com>
2019-08-16 20:01:32 -0400

    SOLVED. One of the tests was filtering out images that weren't gifs or videos in the class variable. So the indices were different than what was in the file

--- tests/test_imgur_interface.py
+++ tests/test_imgur_interface.py
@@ -15,12 +15,14 @@ class TestImgurInterface(unittest.TestCase):
                          'sample-data/rising-gallery.json')
             )
         cls.interface = ii.ImgurInterface()
+    
+    def setUp(self):
         filename = TestImgurInterface.SAMPLE_RISING_GALLERY_RESPONSE_FILENAME
         with open(filename, 'r') as f:
-            cls.sample_rising_gallery_response = json.load(f)
+            self.sample_rising_gallery_response = json.load(f)
     
     def test_image_is_gif(self):
-        should_be_gif = TestImgurInterface.sample_rising_gallery_response['data'][7]
+        should_be_gif = self.sample_rising_gallery_response['data'][13]
         # line 1368 in rising-gallery.json
         # checking title just as a sanity check: is this the post we actually
         # want to test? 
@@ -29,14 +31,14 @@ class TestImgurInterface(unittest.TestCase):
                             should_be_gif['images'][0]))
 
     def test_image_is_video(self):
-        should_be_video = TestImgurInterface.sample_rising_gallery_response['data'][12]
+        should_be_video = self.sample_rising_gallery_response['data'][22]
         # line 2373 in rising-gallery.json
         self.assertEqual(should_be_video['title'], 'Holy Shit')
         self.assertTrue(TestImgurInterface.interface.image_is_gif(
                             should_be_video['images'][0]))
 
     def test_filter_gifs(self):
-        filtered = TestImgurInterface.interface.filter_gifs_from_gallery_response(TestImgurInterface.sample_rising_gallery_response)
+        filtered = TestImgurInterface.interface.filter_gifs_from_gallery_response(self.sample_rising_gallery_response)
         for post in filtered['data']:
             if 'images' in post:
                 for image in post['images']:
