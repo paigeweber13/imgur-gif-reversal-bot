2c952870b845f5a66b68eb47ccd30ab2d32c0379
Riley Weber <rileyw13@protonmail.com>
2019-08-23 11:17:38 -0400

    can now reverse an mp4

--- gif_reverser/core.py
+++ gif_reverser/core.py
@@ -1,10 +1,32 @@
 # -*- coding: utf-8 -*-
 
+import cv2
+
 class GifReverser:
     def __init__(self):
         pass
 
-    def reverse_gif(self, gif):
-        pass
+    def get_frames(self, gif_filename: str):
+        vidcap = cv2.VideoCapture(gif_filename)
+        framerate = vidcap.get(cv2.CAP_PROP_FPS)
+        frames = []
 
-    def reverse_mp4(self, gif):
+        success,image = vidcap.read()
+        while success:
+            frames.append(image)
+            success,image = vidcap.read()
+        return (framerate, frames)
+
+    def reverse_gif(self, gif_filename: str, output_filename: str):
+        framerate, frames = self.get_frames(gif_filename)
+        height, width, layers = frames[0].shape
+        size = (width, height)
+        out = cv2.VideoWriter(output_filename, cv2.VideoWriter_fourcc(*'mp4v'),
+                              framerate, size)
+
+        for i in range(len(frames)-1, -1, -1):
+            out.write(frames[i])
+        out.release()
+
+    def reverse_mp4(self, mp4):
+        pass
