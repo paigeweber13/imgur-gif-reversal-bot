30ddb3f51d8c8598b81e0f45e05ad05b6bbfcdaa
Riley Weber <rileyw13@protonmail.com>
2019-08-27 14:21:18 -0400

    should find time to refresh for every hour Needs testing

--- data_cli.py
+++ data_cli.py
@@ -4,9 +4,17 @@ os.chdir(os.path.abspath(os.path.dirname(__file__)))
 
 from imgur_gif_reversal_bot import data
 
+import json
+
+OUTPUT_FILE = 'output.json'
+
 def main():
-    data.find_current_time_for_full_rising_refresh()
-    pass
+    # data.find_current_time_for_full_rising_refresh()
+    time_taken_by_hour = data.find_hourly_time_to_refresh_rising()
+
+    with open(OUTPUT_FILE) as f:
+        json.dump(time_taken_by_hour, f)
+    data.graph_hourly_time_to_refresh(time_aken_by_hour)
 
 if __name__ == '__main__':
     main()
--- imgur_gif_reversal_bot/data.py
+++ imgur_gif_reversal_bot/data.py
@@ -1,6 +1,10 @@
 import datetime
 import time
 
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
 from .context import imgur_interface
 
 interface = imgur_interface.ImgurInterface()
@@ -22,8 +26,9 @@ def find_current_time_for_full_rising_refresh():
     while(num_common_posts):
         print("number of common posts: ", num_common_posts)
         print('waiting...')
-        time.sleep(73)
-        # time.sleep(60)
+        # minimum amount of time between requests to not exceed daily limit
+        # time.sleep(73)
+        time.sleep(120)
         print('getting rising gifs again')
         # we could get an exception here.... like ConnectionError?
         # https://2.python-requests.org/en/master/_modules/requests/exceptions/
@@ -31,17 +36,43 @@ def find_current_time_for_full_rising_refresh():
         num_common_posts = len(start_ids.intersection(current_ids))
 
     ### TODO:
-    # * set rate to something more regular... every 2 minutes?
-    #   every one minute is dangerous because that's over the amount of
-    #   requests per day I can do.
     # * gather data: number of common posts by minute passed
     # * graph data with seaborne
     end_time = datetime.datetime.now()
     diff = end_time - start_time
     print("time taken to have all new posts:", diff)
+    return diff
 
 def find_hourly_time_to_refresh_rising():
     ### TODO:
-    # implement. This will run 'find_current_time_for...' every hour (?) to
-    # give us some data across a whole day.
-    pass
+    # * implement. This will run 'find_current_time_for...' every hour (?) to
+    #   give us some data across a whole day.
+    # * graph data with seaborne
+    time_taken_by_hour = {}
+
+    for i in range(24):
+        while datetime.datetime.now().minute != 0:
+            print('not first minute of this hour, waiting...')
+            time.sleep(55)
+        
+        ## First minute of every hour!
+        this_hour = datetime.datetime.now().hour
+        print('#### HOUR:', this_hour)
+        print('Finding time taken to refresh rising....')
+        time_taken_by_hour[this_hour] = find_current_time_for_full_rising_refresh()
+        
+    return time_taken_by_hour
+
+def graph_hourly_time_to_refresh(time_taken_by_hour):
+    sns.set(style="white", context="talk")
+    x_hours = []
+    y_times_taken = []
+    for key, value in time_taken_by_hour:
+        x_hours.append(key)
+        y_times_taken.append(value)
+    
+    x_hours = np.array(x_hours)
+    y_times_taken = np.array(y_times_taken)
+
+    plot = sns.barplot(x=x_hours, y=y_times_taken, palette='vlag')
+    plot.savefig('output.png')
