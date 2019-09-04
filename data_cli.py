ad9769eed41773603c7b98770ef457458574cd69
Riley Weber <rileyw13@protonmail.com>
2019-09-04 10:07:46 -0400

    separates data collection and data graphing

--- data_cli.py
+++ data_cli.py
@@ -5,16 +5,29 @@ os.chdir(os.path.abspath(os.path.dirname(__file__)))
 from imgur_gif_reversal_bot import data
 
 import json
+import sys
 
-OUTPUT_FILE = 'data-output.json'
+OUTPUT_FILE = 'data-output.csv'
+HELP_TEXT = 'supply \'c\' for collect or \'g\' for graph as the only argument'
+
+def collect():
+    data.find_current_time_for_refresh_and_save_to_csv(OUTPUT_FILE)
+
+def graph():
+    data.graph_hourly_time_to_refresh(OUTPUT_FILE)
 
 def main():
     # data.find_current_time_for_full_rising_refresh()
-    time_taken_by_hour = data.find_hourly_time_to_refresh_rising()
+    if len(sys.argv) < 2:
+        print(HELP_TEXT)
+        sys.exit(1)
 
-    with open(OUTPUT_FILE) as f:
-        json.dump(time_taken_by_hour, f)
-    data.graph_hourly_time_to_refresh(time_taken_by_hour)
+    if sys.argv[1] == 'c':
+        collect()
+    elif sys.argv[1] == 'g':
+        graph()
+    else:
+        print(HELP_TEXT)
 
 if __name__ == '__main__':
     main()
